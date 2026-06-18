ca9e6a9d16aa0d5efa3dfa932bdfa9e94ee4f7af
Abhijith <abhijithkumar2000@gmail.com>
2026-06-18 20:34:40 +0530

    added productInfo component

--- product-catalog-web/src/features/products/components/ProductInfo.tsx
+++ product-catalog-web/src/features/products/components/ProductInfo.tsx
@@ -1,3 +1,15 @@
-export default function ProductInfo() {
+import type { Product } from "../types";
+
+export default function ProductInfo({ product }: { product: Product }) {
+    console.log(product);
+    return (
+        <div>
+            <h2>{product.name}</h2>
+            <p>{product.shortDescription}</p>
+            <p>Price: Rs. {product.priceInr.toLocaleString("en-IN")}</p>
+            <p>Mask Type: {product.maskType}</p>
+            <p>{product.inStock ? "In stock" : "Out of stock"}</p>
+        </div>
+    )
 
 }
